b1353d5507044287e834346d05b3747de66f1f85
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2020-05-24 19:03:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import inspect
 import datetime
 import time
 import os
+import inspect
 
 client = commands.Bot(command_prefix = "-")
 client.remove_command("help")
@@ -23,5 +24,15 @@ async def on_ready():
     if (int(hour) == 15 and int(min) == 53) or (int(hour) == 9 and int(min) == 53):
       await client.get_channel(714136573079453799).send('<@&714144632111366154>, ñîâñåì ñêîðî ñëóæåíèå!')
       time.sleep(60)
+      
+@client.command() 
+async def ev(message,*command):
+  if message.author.id == 529044574660853761:
+    command = " ".join(command)
+    res = eval(command)
+    if inspect.isawaitable(res): 
+      await message.channel.send('```py\n' + str(await res) + '```')
+    else:
+      await message.channel.send('```py\n' + str(res) + '```')
   
 client.run(tt)
