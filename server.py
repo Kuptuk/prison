7bbe04016c89ce9d4536530ed90630e99c96eda1
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2020-05-24 19:11:15 +0300

    Update server.py

--- server.py
+++ server.py
@@ -19,20 +19,10 @@ async def on_ready():
     min = now.split(":")[1]
     hour = now.split(":")[0].split(" ")[1]
     if int(min) % 10 == 0:
-      await client.get_channel(714136573079453799).send('<@&714138893036814397>, àäñêàÿ øàõòà ñêîðî îáíîâèòñÿ!')
+      await client.get_channel(714136573079453799).send('<@&714138893036814397> скоро обновится!')
       time.sleep(60)
     if (int(hour) == 15 and int(min) == 53) or (int(hour) == 9 and int(min) == 53):
-      await client.get_channel(714136573079453799).send('<@&714144632111366154>, ñîâñåì ñêîðî ñëóæåíèå!')
+      await client.get_channel(714136573079453799).send('<@&714144632111366154> совсем скоро')
       time.sleep(60)
-      
-@client.command() 
-async def ev(message,*command):
-  if message.author.id == 529044574660853761:
-    command = " ".join(command)
-    res = eval(command)
-    if inspect.isawaitable(res): 
-      await message.channel.send('```py\n' + str(await res) + '```')
-    else:
-      await message.channel.send('```py\n' + str(res) + '```')
   
 client.run(tt)
