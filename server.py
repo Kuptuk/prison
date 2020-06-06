3de0beb5dadb17bc0949cdcf5d7b3b5bffb56857
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2020-06-06 17:21:01 +0300

    Update server.py

--- server.py
+++ server.py
@@ -22,6 +22,7 @@ async def on_ready():
     second = now.split(":")[2].split(".")[0]
     if int(min) % 10 == 0:
       await client.get_channel(714136573079453799).send('<@&714138893036814397> скоро обновится!')
+      await client.get_channel(714136573079453799).send('<@&718816998540181604> шахта скоро обновится!')
       time.sleep(60)
     if (int(hour) == 15 and int(min) == 53) or (int(hour) == 9 and int(min) == 53):
       await client.get_channel(714136573079453799).send('<@&714144632111366154> совсем скоро!')
