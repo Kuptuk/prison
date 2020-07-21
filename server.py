fc35e06a94afb53056cfe26327d163d523de3312
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2020-07-21 17:46:17 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,28 +12,23 @@ client.remove_command("help")
 
 tt = os.environ.get("TOKEN")
 
-@client.event
-async def on_ready():
-  await client.change_presence(status=discord.Status.dnd,activity=discord.Game("Снятие тупых модераторов."))
-  while True:
-    now = str(datetime.datetime.now())
-    min = now.split(":")[1]
-    hour = now.split(":")[0].split(" ")[1]
-    second = now.split(":")[2].split(".")[0]
-    if int(min) % 10 == 0:
-      await client.get_channel(714136573079453799).send('<@&714138893036814397> скоро обновится!')
-      if int(min) % 4 == 0:
-        await client.get_channel(714136573079453799).send('<@&718816998540181604> скоро обновится!')
-      time.sleep(60)
-    if (int(hour) == 15 and int(min) == 53) or (int(hour) == 9 and int(min) == 53):
-      await client.get_channel(714136573079453799).send('<@&714144632111366154> совсем скоро!')
-      time.sleep(60)
-    if min == '04':
-      await client.get_channel(714136573079453799).send('<@&714174328488591490> сработали!')
-      await client.get_channel(714136573079453799).send('<@&718816998540181604> скоро обновится!')
-      time.sleep(60)
-    if int(min) % 4 == 0 and int(min) % 10 != 0:
-      await client.get_channel(714136573079453799).send('<@&718816998540181604> скоро обновится!')
-      time.sleep(60)
+@client.command() 
+async def ev(message,*command):
+  if message.author.id == 529044574660853761:
+    command = " ".join(command)
+    res = eval(command)
+    if inspect.isawaitable(res): 
+      await message.channel.send('```py\n' + str(await res) + '```')
+    else:
+      await message.channel.send('```py\n' + str(res) + '```')
       
+@client.command()
+async def check(message, id=None):
+  if id is None:
+    id = message.author.id
+  else:
+    id = id.replace("!", "").replace("@","").replace("<","").replace(">","")
+  a = await client.fetch_user(int(id))
+  await message.channel.send(a.created_at)
+  
 client.run(tt)
