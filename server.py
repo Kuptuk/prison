3c1a94139e93698234b0602a684c3a7489663eae
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2020-07-21 17:50:23 +0300

    Update server.py

--- server.py
+++ server.py
@@ -29,6 +29,6 @@ async def check(message, id=None):
   else:
     id = id.replace("!", "").replace("@","").replace("<","").replace(">","")
   a = await client.fetch_user(int(id))
-  await message.channel.send(f'```css\nДата создания аккаунта {a}: {a.created_at}```')
+  await message.channel.send(f'```css\nДата создания аккаунта {a}: {str(a.created_at).split('.')[0]}```')
   
 client.run(tt)
